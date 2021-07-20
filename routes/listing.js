4e07a7ebbf243e2a8584eb37842becf5149ddf60
chemazu <chemazu>
2021-07-20 20:05:08 +0100

    crud for listing looks good

--- routes/listing.js
+++ routes/listing.js
@@ -7,16 +7,14 @@ const {
   deleteListing,
   getListing,
   getListings,
-  addListing2,
 } = require("../controllers/listing");
 const Listing = require("../models/Listing.model");
 const router = Router();
 
 router.post("/add", addListing);
-// router.post("/add2", addListing2);
 router.put("/update/:id", updateListing);
 router.delete("/delete/:id", deleteListing);
 router.get("/listing/:id", getListing);
-router.get("/listings/:id", getListings); //my listing
+router.get("/listings/:id", getListings);
 
 module.exports = router;
